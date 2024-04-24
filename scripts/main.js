0097000cc1da5622f82e0452860081dcc595e734
Alejandropuentes27 <144942967+Alejandropuentes27@users.noreply.github.com>
2024-04-24 18:37:41 +0000

    edit.html

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+//task 1
+console.log("task 1")
+for (i=1; i <11; i++){
+    console.log(i*i);
+}
+//task 2
+console.log("task 2")
+    for(i=6; i >0; 1--){
+    console.log(i-1);
+}
+console.log("blastoff");
+//task 3
+ console.log("task 3")
+
+console.log("task 4")
