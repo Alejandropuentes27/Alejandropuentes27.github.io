4c33c10a093ed8e07f44b1523fcdc394c3d0ae30
Alejandropuentes27 <144942967+Alejandropuentes27@users.noreply.github.com>
2024-04-26 19:21:16 +0000

    edit.html

--- scripts/main.js
+++ scripts/main.js
@@ -4,12 +4,32 @@ for (i=1; i <11; i++){
     console.log(i*i);
 }
 //task 2
+
 console.log("task 2")
-    for(i=6; i >0; 1--){
-    console.log(i-1);
+    for(i=5; i >= 0; i--){
+    console.log(i);
 }
 console.log("blastoff");
 //task 3
  console.log("task 3")
+    for(i=0; i <= 50; i+= 2){
+        console.log(i)
+    }
+//task 4
 
 console.log("task 4")
+let sum = 0
+    for(let i=0; i <= 100; i++){
+        sum += i
+        console.log(sum)
+    }
+
+//task 5
+console.log("task 5")
+    let factorial = 1;
+    for(let i = 1; i<=10; i++){
+        factorial *= i;
+        console.log(factorial)
+    }
+   
+  
