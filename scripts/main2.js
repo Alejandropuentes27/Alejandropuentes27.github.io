0eda3ecbd9c22849eed07c6a6d7d37e77c8881c4
Alejandropuentes27 <144942967+Alejandropuentes27@users.noreply.github.com>
2024-06-07 06:49:43 +0000

    pre finishing touches

--- scripts/main2.js
+++ scripts/main2.js
@@ -1,3 +1,12 @@
 const button = document.getElementById('button1');
 const link = document.getElementById("nextPage");
 
+
+const images = ['image/primo.jpg', 'image/bear-primo.jpg', 'image/primo-laying-down.jpg', 'image/cool-primo.jpg', 'image/primo-jump.jpg'];
+let currentImage = 0;
+const galleryImage = document.getElementById('galleryImage');
+
+setInterval(() => {
+    galleryImage.src = images[currentImage];
+    currentImage = (currentImage + 1) % images.length;
+}, 3000);
